9293ee581a0d2a9257d029dc00332c3511326990
이성민 <ll8470@naver.com>
2022-05-22 20:36:26 +0900

    .env.loca mapkey & 문서서식통일

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,41 +1,36 @@
 <template>
-  <div class="p-4">
-    <hr />
-    <div class="col-2 float-left">
-      <img src="@/assets/happyhouse.png" alt="" class="col-9" />
-    </div>
-    <div>
-      <h3>Find Us</h3>
-      <div class="line"></div>
-      <div>
-        <ul class="nav flex-column">
-          <li>
-            <i class="fa-solid fa-location-dot"></i> (SSAFY) 서울시 강남구
-            테헤란로 멀티스퀘어
-          </li>
-          <li class="footer_info_item">
-            <i class="fa-solid fa-phone"></i> 1544-9001
-          </li>
-          <li class="footer_info_item">
-            <i class="fa-solid fa-envelope"></i> admin@ssafy.com
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
+	<div class="p-4">
+		<hr />
+		<div class="col-2 float-left">
+			<img src="@/assets/happyhouse.png" alt="" class="col-9" />
+		</div>
+		<div>
+			<h3>Find Us</h3>
+			<div class="line"></div>
+			<div>
+				<ul class="nav flex-column">
+					<li>
+						<i class="fa-solid fa-location-dot"></i> (SSAFY) 서울시 강남구 테헤란로 멀티스퀘어
+					</li>
+					<li class="footer_info_item"><i class="fa-solid fa-phone"></i> 1544-9001</li>
+					<li class="footer_info_item"><i class="fa-solid fa-envelope"></i> admin@ssafy.com</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "VuejsFooter",
+	name: "VuejsFooter",
 
-  data() {
-    return {};
-  },
+	data() {
+		return {};
+	},
 
-  mounted() {},
+	mounted() {},
 
-  methods: {},
+	methods: {},
 };
 </script>
 
